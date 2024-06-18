2679f14aabb17a2c1669c42c99a0947e629112e1
julienbrg <julien@strat.cc>
2024-06-18 11:43:33 +0200

    clean up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,34 +25,6 @@ export default function Home() {
   const provider: Eip1193Provider | undefined = walletProvider
   const toast = useToast()
 
-  const getBal = async () => {
-    if (isConnected) {
-      const ethersProvider = new BrowserProvider(provider as any)
-      const signer = await ethersProvider.getSigner()
-      const balance = await ethersProvider.getBalance(address as any)
-      const ethBalance = ethers.formatEther(balance)
-      if (ethBalance !== '0') {
-        return Number(ethBalance)
-      } else {
-        return 0
-      }
-    } else {
-      return 0
-    }
-  }
-
-  const faucetTx = async () => {
-    const customProvider = new ethers.JsonRpcProvider(process.env.NEXT_PUBLIC_RPC_ENDPOINT_URL)
-    const pKey = process.env.NEXT_PUBLIC_SIGNER_PRIVATE_KEY || ''
-    const specialSigner = new ethers.Wallet(pKey, customProvider)
-    const tx = await specialSigner.sendTransaction({
-      to: address,
-      value: parseEther('0.0001'),
-    })
-    const receipt = await tx.wait(1)
-    return receipt
-  }
-
   const call = async () => {
     setIsLoading(true)
     const response = await fetch('/api/assistant', {
@@ -69,70 +41,6 @@ export default function Home() {
     setIsLoading(false)
   }
 
-  const doSomething = async () => {
-    try {
-      if (!isConnected) {
-        toast({
-          title: 'Not connected yet',
-          description: 'Please connect your wallet, my friend.',
-          status: 'error',
-          position: 'bottom',
-          variant: 'subtle',
-          duration: 9000,
-          isClosable: true,
-        })
-        return
-      }
-      let signer
-      if (provider) {
-        setIsLoading(true)
-        setTxHash('')
-        setTxLink('')
-        const ethersProvider = new BrowserProvider(provider)
-        signer = await ethersProvider.getSigner()
-
-        ///// Send ETH if needed /////
-        const bal = await getBal()
-        console.log('bal:', bal)
-        if (bal < 0.0001) {
-          const faucet = await faucetTx()
-          console.log('faucet tx', faucet)
-        }
-
-        ///// Call /////
-        const erc20 = new Contract(ERC20_CONTRACT_ADDRESS, ERC20_CONTRACT_ABI, signer)
-        const call = await erc20.mint(parseEther('10000'))
-        const receipt = await call.wait()
-
-        console.log('tx:', receipt)
-        setTxHash(receipt.hash)
-        setTxLink('https://sepolia.etherscan.io/tx/' + receipt.hash)
-        setIsLoading(false)
-        toast({
-          title: 'Successful tx',
-          description: 'Well done! 🎉',
-          status: 'success',
-          position: 'bottom',
-          variant: 'subtle',
-          duration: 20000,
-          isClosable: true,
-        })
-      }
-    } catch (e) {
-      setIsLoading(false)
-      console.log('error:', e)
-      toast({
-        title: 'Woops',
-        description: 'Something went wrong...',
-        status: 'error',
-        position: 'bottom',
-        variant: 'subtle',
-        duration: 9000,
-        isClosable: true,
-      })
-    }
-  }
-
   return (
     <>
       <Head title={SITE_NAME} description={SITE_DESCRIPTION} />
@@ -140,15 +48,12 @@ export default function Home() {
         <FormControl>
           <HeadingComponent as={'h1'}>Aïcha, écoute-moi ! </HeadingComponent>
           <br />
-
-          {/* <FormLabel>Demander ce que vous voulez à Aïcha...</FormLabel> */}
           <Textarea value={input} onChange={(e) => setInput(e.target.value)} placeholder="" />
           <FormHelperText>Demander ce que vous voulez à Aïcha...</FormHelperText>
         </FormControl>
 
         <br />
         <Button
-          // mt={7}
           colorScheme="blue"
           variant="outline"
           type="submit"
