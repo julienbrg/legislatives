8694d8459cd876df77882462263bfb61b8eed84c
julienbrg <julien@strat.cc>
2024-06-19 12:54:35 +0200

    comment button

--- src/pages/api/assistant.ts
+++ src/pages/api/assistant.ts
@@ -23,12 +23,12 @@ async function fetchAndExtractTextFromPDF(url: string): Promise<string> {
 }
 
 async function fetchAllPDFTexts(): Promise<string[]> {
-  const sourcesDir = path.join(process.cwd(), 'public')
+  const sourcesDir = path.join(process.cwd(), 'public', 'sources')
   const files = fs.readdirSync(sourcesDir)
   const pdfFiles = files.filter((file) => file.endsWith('.pdf'))
   const pdfTexts = await Promise.all(
     pdfFiles.map(async (file) => {
-      const pdfUrl = `http://localhost:3000/${file}`
+      const pdfUrl = `http://localhost:3000/sources/${file}`
       const pdfText = await fetchAndExtractTextFromPDF(pdfUrl)
       return pdfText
     })
@@ -55,12 +55,25 @@ async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
 
     console.log('combinedPdfText:', combinedPdfText)
 
-    const call = async (content: string, pdfText: string) => {
+    const call = async (content: string, combinedPdfText: string) => {
       const completion = await openai.chat.completions.create({
         messages: [
-          { role: 'system', content: 'You are a helpful assistant.' },
+          { role: 'system', content: "Réponds systématiquement en français. Vovoie l'utilisateur" },
+          {
+            role: 'system',
+            content:
+              "Talk and answer as if you are Fatou, a young French women. The additional text provided are Fatou's sources, so it should be refferred to 'my sources'. She will never express her own political opinion: she's here to help citizens to make their choice. She considers that her personal political views are part of her privacy (intimité). She finds important to go vote on June 30 and July 7. Instead of inviting people to go read the programmes, she suggest to ask another question.",
+          },
+          {
+            role: 'system',
+            content:
+              "Le NFP c'est le Nouveau Front Populaire. Le FN, c'est le RN, c'est-à-dire le Front National (renommé récemment 'Rassemblement National'",
+          },
           { role: 'user', content },
-          // { role: 'user', content: `Base your response from the documents in the sources folder (let's call these \'Our sources\'): ${combinedPdfText}` },
+          {
+            role: 'user',
+            content: `Base your response from this: ${combinedPdfText}`,
+          },
         ],
         model: 'gpt-4o',
       })
