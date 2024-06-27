ea7e34a3ede61ad7c62cee3a2cb0d3f7edfced39
julienbrg <julien@strat.cc>
2024-06-27 18:06:24 +0200

    add hardhat dir

---
+++ gov/contracts/Gov.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/governance/Governor.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorSettings.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
+
+contract Gov is
+    Governor,
+    GovernorSettings,
+    GovernorCountingSimple,
+    GovernorVotes,
+    GovernorVotesQuorumFraction
+{
+    string public manifesto;
+
+    event ManifestoUpdated(string cid);
+
+    constructor(
+        IVotes _token,
+        string memory _manifesto,
+        string memory _name,
+        uint48 _votingDelay,
+        uint32 _votingPeriod,
+        uint256 _votingThreshold,
+        uint256 _quorum
+    )
+        Governor(_name)
+        GovernorSettings(_votingDelay, _votingPeriod, _votingThreshold)
+        GovernorVotes(_token)
+        GovernorVotesQuorumFraction(_quorum)
+    {
+        manifesto = _manifesto;
+    }
+
+    function votingDelay() public view override(Governor, GovernorSettings) returns (uint256) {
+        return super.votingDelay();
+    }
+
+    function votingPeriod() public view override(Governor, GovernorSettings) returns (uint256) {
+        return super.votingPeriod();
+    }
+
+    function quorum(
+        uint256 blockNumber
+    ) public view override(Governor, GovernorVotesQuorumFraction) returns (uint256) {
+        return super.quorum(blockNumber);
+    }
+
+    function proposalThreshold()
+        public
+        view
+        override(Governor, GovernorSettings)
+        returns (uint256)
+    {
+        return super.proposalThreshold();
+    }
+
+    /// @notice Replaces the CID of the manifesto
+    /// @dev Must include the DAO mission statement
+    /// @param cid The CID of the new manifesto
+    function setManifesto(string memory cid) public onlyGovernance {
+        manifesto = cid;
+        emit ManifestoUpdated(cid);
+    }
+}
---
+++ gov/hardhat.config.ts
@@ -0,0 +1,96 @@
+import { HardhatUserConfig } from "hardhat/config"
+import "@nomicfoundation/hardhat-toolbox"
+import "@nomicfoundation/hardhat-verify"
+import "hardhat-deploy"
+import * as dotenv from "dotenv"
+dotenv.config()
+
+const {
+    SEPOLIA_RPC_ENDPOINT_URL,
+    SEPOLIA_PRIVATE_KEY,
+    ETHERSCAN_API_KEY,
+    ARTHERA_TESTNET_RPC_ENDPOINT_URL,
+    ARTHERA_TESTNET_PRIVATE_KEY,
+    OP_SEPOLIA_RPC_ENDPOINT_URL,
+    OP_SEPOLIA_PRIVATE_KEY,
+    OP_ETHERSCAN_API_KEY,
+    ARTHERA_MAINNET_RPC_ENDPOINT_URL,
+    ARTHERA_MAINNET_PRIVATE_KEY
+} = process.env
+
+const config: HardhatUserConfig = {
+    defaultNetwork: "hardhat",
+    namedAccounts: {
+        deployer: 0
+    },
+    networks: {
+        hardhat: {
+            chainId: 1337,
+            allowUnlimitedContractSize: true
+        },
+        arthera: {
+            chainId: 10242,
+            url: ARTHERA_MAINNET_RPC_ENDPOINT_URL || "https://rpc.arthera.net",
+            accounts:
+                ARTHERA_MAINNET_PRIVATE_KEY !== undefined
+                    ? [ARTHERA_MAINNET_PRIVATE_KEY]
+                    : []
+        },
+        sepolia: {
+            chainId: 11155111,
+            url:
+                SEPOLIA_RPC_ENDPOINT_URL ||
+                "https://ethereum-sepolia.publicnode.com",
+            accounts:
+                SEPOLIA_PRIVATE_KEY !== undefined ? [SEPOLIA_PRIVATE_KEY] : []
+        },
+        "op-sepolia": {
+            chainId: 11155420,
+            url:
+                OP_SEPOLIA_RPC_ENDPOINT_URL ||
+                "https://ethereum-sepolia.publicnode.com",
+            accounts:
+                OP_SEPOLIA_PRIVATE_KEY !== undefined
+                    ? [OP_SEPOLIA_PRIVATE_KEY]
+                    : []
+        },
+        "arthera-testnet": {
+            chainId: 10243,
+            url:
+                ARTHERA_TESTNET_RPC_ENDPOINT_URL ||
+                "https://rpc-test.arthera.net",
+            accounts:
+                ARTHERA_TESTNET_PRIVATE_KEY !== undefined
+                    ? [ARTHERA_TESTNET_PRIVATE_KEY]
+                    : []
+        }
+    },
+    solidity: {
+        version: "0.8.25",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200
+            },
+            evmVersion: "shanghai"
+        }
+    },
+    etherscan: {
+        apiKey: {
+            sepolia: ETHERSCAN_API_KEY || "",
+            "op-sepolia": OP_ETHERSCAN_API_KEY || ""
+        },
+        customChains: [
+            {
+                network: "op-sepolia",
+                chainId: 11155420,
+                urls: {
+                    apiURL: "https://api-sepolia-optimistic.etherscan.io/api",
+                    browserURL: "https://sepolia-optimism.etherscan.io"
+                }
+            }
+        ]
+    }
+}
+
+export default config
