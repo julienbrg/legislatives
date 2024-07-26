170fcb523b2f4583e2fb19f3f45f9da8f621b33f
julienbrg <julien@strat.cc>
2024-07-26 15:37:12 +0200

    clean up

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import { Flex, useColorModeValue, Spacer, Heading, Menu, MenuButton, MenuList, MenuItem, IconButton } from '@chakra-ui/react'
-import { AddIcon, ExternalLinkIcon, RepeatIcon, EditIcon, HamburgerIcon, ArrowBackIcon, InfoIcon } from '@chakra-ui/icons'
-import { FaEthereum, FaEuroSign, FaHome, FaFeather, FaVoteYea } from 'react-icons/fa'
+import { HamburgerIcon } from '@chakra-ui/icons'
+import { FaHome, FaVoteYea } from 'react-icons/fa'
 import { LinkComponent } from './LinkComponent'
 import { ThemeSwitcher } from './ThemeSwitcher'
 import { SITE_NAME } from '../../utils/config'
-import { GiReceiveMoney } from 'react-icons/gi'
-import { IoExitOutline, IoEnterOutline } from 'react-icons/io5'
-import { GrValidate } from 'react-icons/gr'
 import { LuBookOpenCheck, LuKeyRound } from 'react-icons/lu'
 
 interface Props {
@@ -32,7 +29,7 @@ export function Header(props: Props) {
           <MenuList>
             <LinkComponent href="/">
               <MenuItem fontSize="xl" icon={<FaHome />}>
-                Demander à Fatou
+                Comparateur
               </MenuItem>
             </LinkComponent>
             <LinkComponent href="/sources">
@@ -43,20 +40,20 @@ export function Header(props: Props) {
             {/* <LinkComponent href="/colere">
               <MenuItem fontSize="xl">Colère</MenuItem>
             </LinkComponent> */}
-            {/* <LinkComponent href="/mon-vote">
+            <LinkComponent href="/mon-vote">
               <MenuItem fontSize="xl" icon={<FaVoteYea />}>
                 Mon vote
               </MenuItem>
-            </LinkComponent> */}
+            </LinkComponent>
             <LinkComponent href="/gouv">
               <MenuItem fontSize="xl" icon={<LuKeyRound />}>
-                Mon gouv
+                Mes réformes
               </MenuItem>
             </LinkComponent>
           </MenuList>
         </Menu>
-        {/* <w3m-button /> */}
         <ThemeSwitcher />
+        <w3m-button />
       </Flex>
     </Flex>
   )
